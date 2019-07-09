a39daf03f12957d70e2a85aebf30e28d29023fcc
Karol Czernicki <karol.czernicki@silksh.pl>
2019-07-09 13:55:37 +0200

    Webpack, ts, basic architecture.

--- README.md
+++ README.md
@@ -1,3 +1,19 @@
 # Weather Forecast App
 
 React + typescript 
+
+### Installation
+
+Install the dependencies and devDependencies and start the server.
+
+```sh
+$ npm install 
+$ npm run-script start
+```
+
+For production environments...
+
+```sh
+$ npm install 
+$ npm run-script build
+```
---
+++ src/app.tsx
@@ -0,0 +1,16 @@
+import * as React from 'react';
+
+import { Application } from './interfaces/AppInterface';
+
+class App extends React.Component<Application, {}> {
+  render() {
+    return (
+      <div>
+        <h1>Welcome to React with Typescript</h1>
+        <p>The webapp name: {this.props.appName}</p>
+      </div>
+  );
+  }
+}
+
+export default App;
